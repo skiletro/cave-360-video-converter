9af3bb496bd004ca15d8b9e124533a667dbad601
skiletro <19377854+skiletro@users.noreply.github.com>
2025-09-29 20:19:30 +0100

    add windows cross compilation

--- flake.nix
+++ flake.nix
@@ -9,19 +9,65 @@
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [
         "aarch64-darwin"
+        "x86_64-linux"
       ];
 
       perSystem =
-        { pkgs, ... }:
+        { pkgs, lib, ... }:
         {
           devShells.default = pkgs.mkShell {
             buildInputs = with pkgs; [
               go
               gopls
               ffmpeg_8
-              apple-sdk_14
+              pkg-config
+              gtk3
+              glfw
             ];
           };
+          packages =
+            let
+              pname = "cave-360-video-converter";
+              version = "0.1.0";
+              src = ./.;
+              vendorHash = "sha256-wUFN6/vQ41Orobryr81MoDlnQ3vK3mspg+bhI0vD9C0=";
+
+              inherit (pkgs.stdenvNoCC.hostPlatform) isLinux isDarwin;
+            in
+            {
+              default = pkgs.buildGoModule {
+                inherit
+                  pname
+                  version
+                  src
+                  vendorHash
+                  ;
+
+                nativeBuildInputs = [ pkgs.ffmpeg_8 ];
+
+                buildInputs =
+                  lib.optionals isLinux (
+                    with pkgs;
+                    [
+                      glfw
+                      pkg-config
+                      gtk3
+                    ]
+                  )
+                  ++ lib.optionals isDarwin [
+                    pkgs.apple-sdk_14
+                  ];
+              };
+
+              windows = pkgs.pkgsCross.mingwW64.buildGoModule {
+                inherit
+                  pname
+                  version
+                  src
+                  vendorHash
+                  ;
+              };
+            };
         };
     };
 }
